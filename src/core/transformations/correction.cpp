75d15af99514693a9dd2a0c6468006d3844f4b06
Marek <mr46179@st.amu.edu.pl>
2015-10-15 14:34:28 +0200

    Korekcja jasności, kontrastu i gamma

--- src/core/transformations/correction.cpp
+++ src/core/transformations/correction.cpp
@@ -1,5 +1,7 @@
 #include "correction.h"
-
+#include <math.h>
+#include <algorithm>
+using namespace std;
 Correction::Correction(PNM* img) :
     Transformation(img)
 {
@@ -8,6 +10,25 @@ Correction::Correction(PNM* img) :
 Correction::Correction(PNM* img, ImageViewer* iv) :
     Transformation(img, iv)
 {
+}
+
+template <typename T>
+T range(T min, T val, T max)
+{
+    T result;
+    if (val < min)
+    {
+        result = min;
+    }
+    else if (min < val && val < max)
+    {
+        result = val;
+    }
+    else
+    {
+        result = max;
+    }
+    return result;
 }
 
 PNM* Correction::transform()
@@ -23,7 +44,9 @@ PNM* Correction::transform()
 
     for (int i = PIXEL_VAL_MIN; i <= PIXEL_VAL_MAX; i++)
     {
-        LUT[i] = i;
+        LUT[i] = range((float)0, i+shift, (float)255);
+        LUT[i] = range((float)0,  LUT[i]*factor, (float)255);
+        LUT[i] = range((float)0,  (float)pow(LUT[i], gamma), (float)255);
     }
 
     for (int x=0; x<width; x++)
@@ -35,6 +58,11 @@ PNM* Correction::transform()
             int r = qRed(pixel);    // Get the 0-255 value of the R channel
             int g = qGreen(pixel);  // Get the 0-255 value of the G channel
             int b = qBlue(pixel);
+			r = LUT[r];
+			g = LUT[g];
+			b = LUT[b];
+			QColor newPixel = QColor(r,g,b);
+            newImage->setPixel(x,y, newPixel.rgb());
         }
     }
 
