4c1830e763a77e88fe29fc8672f3e0171bd2be34
Marek <mr46179@st.amu.edu.pl>
2016-01-14 15:15:48 +0100

    Zadanie 12: Narożniki Harrisa

--- src/core/transformations/corner_harris.cpp
+++ src/core/transformations/corner_harris.cpp
@@ -14,6 +14,48 @@ CornerHarris::CornerHarris(PNM* img, ImageViewer* iv) :
 {
 }
 
+ void CornerHarris::fill_matrices(int width, int height)
+ {
+    I_xx = math::matrix<float>(width, height);
+    I_xy = math::matrix<float>(width, height);
+    I_yy = math::matrix<float>(width, height);
+    candids = math::matrix<float>(width, height);
+    nonmax_sup = math::matrix<float>(width, height);
+ }
+
+ PNM* CornerHarris::toGrayScale(PNM* img)
+ {
+     ConversionGrayscale* cg;
+     PNM* result;
+
+     cg = new ConversionGrayscale(img);
+     result = cg->transform();
+
+     return result;
+ }
+
+ PNM* CornerHarris::toBlurGaussian(PNM* img)
+ {
+     BlurGaussian* bg;
+     PNM* result;
+
+     bg = new BlurGaussian(img);
+     bg->setParameter("size", 3);
+     bg->setParameter("sigma", 1.6);
+     result = bg->transform();
+
+     return result;
+ }
+
+ void CornerHarris::getGradients(PNM* img)
+ {
+     EdgeSobel* es;
+
+     es = new EdgeSobel(img);
+     this->Gx = es->rawHorizontalDetection();
+     this->Gy = es->rawVerticalDetection();
+ }
+
 PNM* CornerHarris::transform()
 {
     int    threshold    = getParameter("threshold").toInt();
@@ -23,10 +65,12 @@ PNM* CornerHarris::transform()
 
     int width  = image->width(),
         height = image->height();
+    PNM* img;
 
     PNM* newImage = new PNM(width, height, QImage::Format_Mono);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
+    this->fill_matrices(width, height);
+    img = this->toGrayScale(this->image);
+    this->getGradients(img);
     return newImage;
 }
--- src/core/transformations/corner_harris.h
+++ src/core/transformations/corner_harris.h
@@ -10,6 +10,12 @@ public:
     CornerHarris(PNM*, ImageViewer*);
 
     PNM* transform();
+private:
+    math::matrix<float> I_xx, I_xy, I_yy, candids, nonmax_sup, *Gx, *Gy;
+    void fill_matrices(int w, int h);
+    PNM* toGrayScale(PNM* img);
+    PNM* toBlurGaussian(PNM* img);
+    void getGradients(PNM* img);
 };
 
 #endif // CORNER_HARRIS_H
