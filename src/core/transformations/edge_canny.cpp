6198dcc1ffd86bd3e4a3eb1d498deda06fec9371
Marek <mr46179@st.amu.edu.pl>
2016-01-21 15:10:59 +0100

    Zadanie 10: Canny - poprawki

--- src/core/transformations/edge_canny.cpp
+++ src/core/transformations/edge_canny.cpp
@@ -25,6 +25,8 @@ PNM* EdgeCanny::toGrayScale(PNM* img)
     cg = new ConversionGrayscale(img);
     result = cg->transform();
 
+    delete cg;
+
     return result;
 }
 
@@ -38,6 +40,8 @@ PNM* EdgeCanny::toBlurGaussian(PNM* img)
     bg->setParameter("sigma", 1.6);
     result = bg->transform();
 
+    delete bg;
+
     return result;
 }
 
@@ -50,21 +54,24 @@ void EdgeCanny::getGradients(int width, int height)
     this->Gy = es->rawVerticalDetection();
     this->magnitude = math::matrix<float>(width, height);
     this->direction = math::matrix<float>(width, height);
+
+    delete es;
 }
 
-int getDirection(float d)
+int getDirection(float o)
 {
-    float degrees = d*100;
-    if ((degrees > 0 && degrees <= 22.5) || (degrees > 157.5 && degrees <= 180))
-        return 0;
-    if (degrees > 22.5 && degrees <= 67.5)
-        return 1;
-    if (degrees > 67.5 && degrees <= 112.5)
-        return 2;
-    if (degrees > 112.5 && degrees <= 157.5)
-        return 3;
-
-    return 0;
+    if ((o < 22.5) || (o >= 157.5 && o < 202.5) || (o >= 337.5)) {
+            return 0;
+        }
+        else if ((o >= 22.5 && o < 67.5) || (o >= 202.5 && o < 247.5)) {
+            return 1;
+        }
+        else if ((o >= 67.5 && o < 112.5) || (o >= 247.5 && o < 292.5)) {
+            return 2;
+        }
+        else if ((o >= 112.5 && o < 157.5) || (o >= 292.5 && o < 337.5)) {
+            return 3;
+        }
 }
 
 void EdgeCanny::calcGradAndDir()
@@ -78,7 +85,12 @@ void EdgeCanny::calcGradAndDir()
         {
             this->newImage->setPixel(i, j, PIXEL_VAL_MIN);
             this->magnitude(i,j) = sqrt(pow((*Gx)(i,j),2) + pow((*Gy)(i,j),2));
-            this->direction(i,j) = atan(abs((*Gy)(i,j))/abs((*Gx)(i,j)));
+            float arctan = atan2(abs((*Gy)(i,j)), abs((*Gx)(i,j)))/M_PI;
+            if (arctan > 0)
+            {
+                arctan = -arctan + 180;
+            }
+            this->direction(i, j) = arctan;
         }
     }
 }
@@ -90,26 +102,27 @@ void EdgeCanny::classifyPixels()
     int i, j;
 
 
-    for (i = 0; i < width; i++)
-        for (j = 0; j < height; j++)
+    for (i = 1; i < width - 1; i++)
+    {
+        for (j = 1; j < height - 1; j++)
         {
             int d = getDirection(this->direction(i,j));
 
             int diff_s1[2], diff_s2[2]; //i,j dla sasiad1, sasiad2 (s1_i, s1_j, s2_i, s2_j - roznica od wyjsciowych i,j
             switch(d)
             {
-            case 0:
-                diff_s1[0] = -1; diff_s1[1] = 0;
-                diff_s2[0] = 1; diff_s2[1] = 0;
-            case 1:
-                diff_s1[0] = 1; diff_s1[1] = 1;
-                diff_s2[0] = -1; diff_s2[1] = 1;
-            case 2:
-                diff_s1[0] = 0; diff_s1[1] = 1;
-                diff_s2[0] = 0; diff_s2[1] = -1;
-            case 3:
-                diff_s1[0] = -1; diff_s1[1] = -1;
-                diff_s2[0] = 1; diff_s2[1] = 1;
+                case 0:
+                    diff_s1[0] = -1; diff_s1[1] = 0;
+                    diff_s2[0] = 1; diff_s2[1] = 0;
+                case 1:
+                    diff_s1[0] = 1; diff_s1[1] = 1;
+                    diff_s2[0] = -1; diff_s2[1] = 1;
+                case 2:
+                    diff_s1[0] = 0; diff_s1[1] = 1;
+                    diff_s2[0] = 0; diff_s2[1] = -1;
+                case 3:
+                    diff_s1[0] = -1; diff_s1[1] = -1;
+                    diff_s2[0] = 1; diff_s2[1] = 1;
             }
 
             if ((i+diff_s1[0] >= 0 && i+diff_s1[0] < width && j+diff_s1[1] >= 0 && j+diff_s1[1] < width
@@ -121,33 +134,37 @@ void EdgeCanny::classifyPixels()
                 this->newImage->setPixel(i, j, PIXEL_VAL_MAX);
                 this->initialEdges.push_back(QPoint(i,j));
             }
-
-           while(!this->initialEdges.empty())
-           {
-               QPoint currentEdge = this->initialEdges.back();
-               this->initialEdges.pop_back();
-               if (this->hysteresis(currentEdge.x(), currentEdge.y(), i, j, d))
-                   newImage->setPixel(currentEdge.x(), currentEdge.y(), PIXEL_VAL_MAX);
-
-           }
         }
+    }
 
+   while(!this->initialEdges.empty())
+   {
+       QPoint currentEdge = this->initialEdges.back();
+       this->initialEdges.pop_back();
+       int x = currentEdge.x(), y = currentEdge.y();
+       int d = getDirection(this->direction(x,y));
+       if (this->hysteresis(x, y, 0, 0, d))
+       {
+           newImage->setPixel(x, y, PIXEL_VAL_MAX);
+       }
+
+   }
 }
+
 
 
 bool EdgeCanny::hysteresis(int x, int y, int x_neighbour, int y_neighbour, int direction_neighbour)
 {
-   
         if (this->magnitude(x,y) < this->lower_thresh)
             return false;
 
         if (getDirection(this->direction(x,y)) != direction_neighbour)
             return false;
 
-        for (int i = -1; i < 2; i++) {
+        for (int i = -1; i <= 1; i++) {
             if (x+i < 0 || x+i < 255)
                 break;
-            for (int j = -1; j < 2; j++) {
+            for (int j = -1; j <= 1; j++) {
                 if (y+j < 0 || y+j > 255)
                     break;
                 if (x+i == x_neighbour && y+j == y_neighbour)
